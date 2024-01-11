626703e879f2cf3818c99df54effebf8f5e8f7e0
shakil-1318 <shakiljust21@gmail.com>
2024-01-11 21:51:49 +0600

    service-finder

---
+++ routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\Customer\CustomerDashboardComponent;
+use App\Http\Livewire\Sprovider\SproviderDashboardComponent;
+use App\Http\Livewire\Admin\AdminDashboardComponent;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', HomeComponent::class)->name('home');
+
+// For Customer
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/customer/dashboard', CustomerDashboardComponent::class)->name('customer.dashboard');
+});
+
+
+
+// For Service Provider
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+    'authsprovider',
+])->group(function () {
+    Route::get('/sprovider/dashboard', SproviderDashboardComponent::class)->name('sprovider.dashboard');
+});
+
+
+
+// Admin
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+    'authadmin',
+])->group(function () {
+    Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+});
